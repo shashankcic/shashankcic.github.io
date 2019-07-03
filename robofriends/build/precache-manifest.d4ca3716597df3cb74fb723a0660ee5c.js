fe923471c49d0e21e47397f17a8e7c905a94d70b
Shashank Singh <48752899+shashankcic@users.noreply.github.com>
2019-07-04 01:51:09 +0530

    Adding files for React

---
+++ robofriends/build/precache-manifest.d4ca3716597df3cb74fb723a0660ee5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aa486d106ff2f870f6883ccae60023",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "025007cf869dbdd00f47",
+    "url": "/robofriends/static/css/main.321bdc19.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "025007cf869dbdd00f47",
+    "url": "/robofriends/static/js/main.32b2dc0f.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
